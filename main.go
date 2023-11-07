98767ca95f852fbb5bb74111ab41defab621d540
ruegerj <jan.rueger@gmx.ch>
2023-11-07 17:09:30 +0100

    add main cli art

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`          ____   _____   ___   ___ ___  ____  `)
+	fmt.Println(`    /\   / __ \ / ____| |__ \ / _ \__ \|___ \ `)
+	fmt.Println(`   /  \ | |  | | |         ) | | | | ) | __) |`)
+	fmt.Println(`  / /\ \| |  | | |        / /| | | |/ / |__ < `)
+	fmt.Println(` / ____ \ |__| | |____   / /_| |_| / /_ ___) |`)
+	fmt.Println(`/_/    \_\____/ \_____| |____|\___/____|____/ `)
+	fmt.Println("-------------------------------")
+	fmt.Println("🎄 Happy Coding & festive season")
+}
