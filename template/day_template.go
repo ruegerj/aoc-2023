a9d2bb670139763a92b2efc9b518da7fce134a1f
ruegerj <jan.rueger@gmx.ch>
2023-11-14 14:32:13 +0100

    fix day methods in template

--- template/day_template.go
+++ template/day_template.go
@@ -4,10 +4,10 @@ import "github.com/ruegerj/aoc-2023/util"
 
 type Day$DAY_NR struct{}
 
-func (d Day01) Part1(input string) *util.Solution {
+func (d Day$DAY_NR) Part1(input string) *util.Solution {
 	return util.NewSolution(1, -1)
 }
 
-func (d Day01) Part2(input string) *util.Solution {
+func (d Day$DAY_NR) Part2(input string) *util.Solution {
 	return util.NewSolution(2, -1)
 }
