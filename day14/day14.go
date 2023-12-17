7fc4138df067cf3a6ab658f84968d19593c8f0bc
ruegerj <jan.rueger@gmx.ch>
2023-12-17 17:11:46 +0100

    day 14 (2)

--- day14/day14.go
+++ day14/day14.go
@@ -2,6 +2,8 @@ package day14
 
 import (
 	"slices"
+	"sort"
+	"strings"
 
 	"github.com/ruegerj/aoc-2023/pkg/common"
 	"github.com/ruegerj/aoc-2023/pkg/util"
@@ -15,46 +17,92 @@ type Day14 struct{}
 
 func (d Day14) Part1(input string) *common.Solution {
 	matrix := util.Matrix(input, "")
-	transposedMatrix := util.Transpose(matrix)
+	matrix = util.Transpose(matrix)
 
-	totalLoad := 0
-
-	for _, row := range transposedMatrix {
-		slices.Reverse(row)
-
-		rollingRocks := make([]string, 0)
-
-		for i, current := range row {
-			if current == EMPTY {
-				continue
-			}
-
-			if current == ROUND {
-				rollingRocks = append(rollingRocks, current)
-				continue
-			}
-
-			totalLoad += calcLoad(rollingRocks, i)
-			rollingRocks = make([]string, 0)
-		}
-
-		totalLoad += calcLoad(rollingRocks, len(row))
+	for i, row := range matrix {
+		matrix[i] = rollLeft(row)
 	}
 
-	return common.NewSolution(1, totalLoad)
+	northLoad := calcNorthLoad(matrix, true)
+
+	return common.NewSolution(1, northLoad)
 }
 
 func (d Day14) Part2(input string) *common.Solution {
-	return common.NewSolution(2, -1)
-}
+	matrix := util.Matrix(input, "")
 
-func calcLoad(rocks []string, maxLevel int) int {
-	load := 0
+	// the pattern shifts due to the rotations end up in a cycle
+	// after 1000 rotations one can be certain that this cycle is reached due to the architecture of the input
+	for i := 0; i < 1000; i++ {
+		// north
+		matrix = util.Transpose(matrix)
+		for i, row := range matrix {
+			matrix[i] = rollLeft(row)
+		}
 
-	for i := 0; i < len(rocks); i++ {
-		load += maxLevel
-		maxLevel--
+		// west
+		matrix = util.Transpose(matrix)
+		for i, row := range matrix {
+			matrix[i] = rollLeft(row)
+		}
+
+		// south
+		matrix = util.Transpose(matrix)
+		for i, row := range matrix {
+			matrix[i] = rollRight(row)
+		}
+
+		// east
+		matrix = util.Transpose(matrix)
+		for i, row := range matrix {
+			matrix[i] = rollRight(row)
+		}
 	}
 
-	return load
+	northLoad := calcNorthLoad(matrix, false)
+
+	return common.NewSolution(2, northLoad)
+}
+
+func rollRight(fields []string) []string {
+	field := strings.Join(fields, "")
+	parts := strings.Split(field, CUBE)
+
+	for i, part := range parts {
+		rocks := strings.Split(part, "")
+		slices.Sort(rocks)
+		parts[i] = strings.Join(rocks, "")
+	}
+
+	return strings.Split(strings.Join(parts, CUBE), "")
+}
+
+func rollLeft(fields []string) []string {
+	field := strings.Join(fields, "")
+	parts := strings.Split(field, CUBE)
+
+	for i, part := range parts {
+		rocks := strings.Split(part, "")
+		sort.Sort(sort.Reverse(sort.StringSlice(rocks)))
+		parts[i] = strings.Join(rocks, "")
+	}
+
+	return strings.Split(strings.Join(parts, CUBE), "")
+}
+
+func calcNorthLoad(matrix [][]string, transposed bool) int {
+	northLoad := 0
+	if !transposed {
+		matrix = util.Transpose(matrix)
+	}
+
+	for _, row := range matrix {
+		for i, rock := range row {
+			if rock == ROUND {
+				northLoad += len(row) - i
+			}
+		}
+	}
+
+	return northLoad
 }
